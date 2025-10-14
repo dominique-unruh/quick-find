d48b05a99a7f3ee45f39032ffc8bbe9e0429c4eb
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2025-10-14 12:10:16 +0200

    FakeRoot: Simple fake item root for testing purposes

---
+++ src/main/scala/items/FakeRoot.scala
@@ -0,0 +1,23 @@
+package de.unruh.quickfind
+package items
+
+import core.{ChildItem, Item, ScalableImage}
+
+class FakeRoot(create: Item => ChildItem) extends Item {
+  override def defaultAction(): Unit = {}
+  override val children: Iterable[ChildItem] = Seq(create(this))
+  override def title: String = "fake root"
+  override def previewLine: String = "fake root"
+  override val parentOption: Option[Item] = None
+  override def icon: ScalableImage = Item.defaultIcon
+  override val persistentKey: Array[Byte] = Array.empty
+}
+
+object FakeRoot {
+  /** Allows to instantiate an item for testing purpuses. */
+  def itemInRoot(create: Item => ChildItem): ChildItem =
+    FakeRoot(create).children.head
+  /** Allows to instantiate and run ([[defaultAction]]) an item for testing purposes. */
+  def runItemInRoot(create: Item => ChildItem): Unit =
+    itemInRoot(create).defaultAction()
+}
